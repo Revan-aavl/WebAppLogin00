b01a73072ecc601af4734d1db2c216480c84f58c
DELL <DELL@DESKTOP-5MKN6RI>
2022-01-27 09:46:38 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ContaController.cs
@@ -0,0 +1,44 @@
+﻿using WebAppLogin00.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace WebAppLogin00.Controllers
+{
+    public class ContaController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginViewModel login, string retunUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(login);
+            }
+
+            var achou = UsuarioModel.ValidarUsuario(login.Usuario, login.Senha);
+
+            if (achou)
+            {
+                FormsAuthentication.SetAuthCookie(login.Usuario, login.LembrarMe);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Login inválido");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebAppLogin00.Models
+{
+    public class UsuarioModel
+    {
+        public static bool ValidarUsuario(string login, string senha)
+        {
+            var ret = false;
+
+            using (var conexao = new SqlConnection())
+            {
+                conexao.ConnectionString = @"Data Source=DESKTOP-5MKN6RI\SQLEXPRESS;Initial Catalog=logindb;Integrated Security=True";
+                conexao.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexao;
+                    comando.CommandText = string.Format(
+                        "select count(*) from usuario where login='{0}' and senha='{1}'", login, senha);
+                    ret = ((int)comando.ExecuteScalar() > 0);
+                }
+            }
+
+            return ret;
+        }
+    }
+}
